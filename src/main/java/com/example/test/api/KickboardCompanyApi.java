123c3160bd1febc572950b2bf4545db34a5eb730
changhwan <korea3611@naver.com>
2021-07-29 15:36:15 +0900

    api수정

--- src/main/java/com/example/test/api/KickboardCompanyApi.java
+++ src/main/java/com/example/test/api/KickboardCompanyApi.java
@@ -42,31 +42,49 @@ public class KickboardCompanyApi {
 
                 KickboardLocation kickboardLocation1 = new KickboardLocation();
                 kickboardLocation1.setCompany_name("씽씽");
-                kickboardLocation1.setLat(37.566571);
-                kickboardLocation1.setLng(126.978441);
+                kickboardLocation1.setLat(37.55377475931086);
+                kickboardLocation1.setLng(126.96435101606421);
                 kickboardLocation1.setBattery(80L);
                 kickboardLocation1.setModel("AAAAA");
                 kickboardLocation1.setImg_url("http://****.jpg");
 
                 KickboardLocation kickboardLocation2 = new KickboardLocation();
                 kickboardLocation2.setCompany_name("킥고잉");
-                kickboardLocation2.setLat(37.566572);
-                kickboardLocation2.setLng(126.978442);
+                kickboardLocation2.setLat(37.54994713018562);
+                kickboardLocation2.setLng(126.96518786529317);
                 kickboardLocation2.setBattery(90L);
                 kickboardLocation2.setModel("CCCCC");
                 kickboardLocation2.setImg_url("http://****.jpg");
 
                 KickboardLocation kickboardLocation3 = new KickboardLocation();
                 kickboardLocation3.setCompany_name("라임");
-                kickboardLocation3.setLat(37.566573);
-                kickboardLocation3.setLng(126.978443);
+                kickboardLocation3.setLat(37.54887535880441);
+                kickboardLocation3.setLng(126.97550900578369);
                 kickboardLocation3.setBattery(100L);
                 kickboardLocation3.setModel("BBBBB");
                 kickboardLocation3.setImg_url("http://****.jpg");
 
+                KickboardLocation kickboardLocation4 = new KickboardLocation();
+                kickboardLocation4.setCompany_name("씽씽");
+                kickboardLocation4.setLat(37.554063949968516);
+                kickboardLocation4.setLng(126.97791226510789);
+                kickboardLocation4.setBattery(70L);
+                kickboardLocation4.setModel("DDDDD");
+                kickboardLocation4.setImg_url("http://****.jpg");
+
+                KickboardLocation kickboardLocation5 = new KickboardLocation();
+                kickboardLocation5.setCompany_name("킥고잉");
+                kickboardLocation5.setLat(37.55843569554293);
+                kickboardLocation5.setLng(126.97276242369888);
+                kickboardLocation5.setBattery(60L);
+                kickboardLocation5.setModel("EEEEE");
+                kickboardLocation5.setImg_url("http://****.jpg");
+
                 kickboardlocation.add(kickboardLocation1);
                 kickboardlocation.add(kickboardLocation2);
                 kickboardlocation.add(kickboardLocation3);
+                kickboardlocation.add(kickboardLocation4);
+                kickboardlocation.add(kickboardLocation5);
 
                 return kickboardlocation;
 
